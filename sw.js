66cacb9deb6c7646e9cbf7adb363e388fdfa91be
Calvin Metcalf <calvin.metcalf@gmail.com>
2016-06-08 19:10:26 -0400

    update

--- sw.js
+++ sw.js
@@ -11,3 +11,15 @@ this.addEventListener('install', function(event) {
     })
   );
 });
+this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('v1').then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  });
+});
